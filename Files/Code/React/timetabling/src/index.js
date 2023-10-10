7b840638e42a3ca80ba5cd9cd3edba8b28c631ae
jvfd3 <joaovitorfd2000@gmail.com>
2023-10-10 13:22:49 -0300

    main CRUD page roughly Done

--- Files/Code/React/timetabling/src/index.js
+++ Files/Code/React/timetabling/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import MainCRUD from "./code/main_CRUD";
+import MainCRUD from "./code/CRUD_main";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
