df1211fbc0360a8adc4dc325aa1baba2ddb38218
jvfd3 <joaovitorfd2000@gmail.com>
2023-10-17 12:49:21 -0300

    Sorting index

--- Files/Code/React/timetabling/src/index.js
+++ Files/Code/React/timetabling/src/index.js
@@ -1,23 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import MainCRUD from "./code/pages/CRUD_main";
-import CRUDdisciplinas from "./code/pages/CRUD_disciplinas";
-import CRUDprofessors from "./code/pages/CRUD_professors";
-import CRUDrooms from "./code/pages/CRUD_rooms";
-import CRUDstudents from "./code/pages/CRUD_students";
-import CRUDclass from "./code/pages/CRUD_class";
-import MainPage from "./code/pages/mainpage";
+import App from "./code/routingWeb";
+// import Routing from "./code/routing";
+// import MainPage from "./code/pages/mainpage";
+// import MainCRUD from "./code/pages/CRUD_main";
+// import CRUDrooms from "./code/pages/CRUD_rooms";
+// import CRUDclass from "./code/pages/CRUD_class";
+// import CRUDstudents from "./code/pages/CRUD_students";
+// import CRUDprofessors from "./code/pages/CRUD_professors";
+// import CRUDdisciplinas from "./code/pages/CRUD_disciplinas";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <App />
+    {/* <Routing /> */}
     {/* <MainCRUD /> */}
-    {/* <CRUDdisciplinas /> */}
-    {/* <CRUDprofessors /> */}
-    {/* <CRUDrooms /> */}
-    <CRUDstudents />
-    {/* <CRUDclass /> */}
     {/* <MainPage /> */}
+    {/* <CRUDclass /> */}
+    {/* <CRUDrooms /> */}
+    {/* <CRUDstudents /> */}
+    {/* <CRUDprofessors /> */}
+    {/* <CRUDdisciplinas /> */}
   </React.StrictMode>
 );
